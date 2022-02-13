e4700422f3a3e700be9689253e2e58b6d81f8caf
Joel Veloz <joevpazm@espol.edu.ec>
2022-02-13 00:25:22 -0300

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return Inertia::render('Mostrar', compact('products'));
+    }
+
+    public function create()
+    {
+        return Inertia::render('FormCrear');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'description' => 'required',
+            'price' => 'required',
+        ]);
+        Product::create($request->all());
+        return Redirect::route('products.index');
+    }
+
+    public function show(Product $product)
+    {
+        //
+    }
+
+    public function edit(Product $product)
+    {
+        return Inertia::render('FormEditar', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return Redirect::route('products.index');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return Redirect::route('products.index');
+    }
+}
---
+++ resources/js/Pages/Product/Mostrar.vue
@@ -0,0 +1,11 @@
+<template>
+  <app-layout> 
+      <h1>
+          Todos los Productos
+      </h1>
+  </app-layout>
+</template>
+
+<script>
+export default {};
+</script>
